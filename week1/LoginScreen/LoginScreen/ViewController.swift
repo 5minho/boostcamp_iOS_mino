5fd9db4ecc63cd5dc45d9b6b3dd954748167a41d
minomi <5minhominho@gmail.com>
2017-07-02 13:50:59 +0900

    버튼 이벤트 추가

--- week1/LoginScreen/LoginScreen/ViewController.swift
+++ week1/LoginScreen/LoginScreen/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtID: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signIn(_ sender: UIButton) {
+        guard let id = txtID.text else {return}
+        guard let pw = txtPassword.text else {return}
+        
+        print("touch up inside - sign in")
+        print("ID : " + id + ", PW : " + pw)
+    }
+    @IBAction func signUp(_ sender: UIButton) {
+        print("touch up inside - sign up")
+    }
 }
 
