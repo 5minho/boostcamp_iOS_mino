2e61fdc7d74587d6424ab659a6a6b93c12c480cc
minomi <5minhominho@gmail.com>
2017-07-11 17:42:29 +0900

    6장 금메달 과제

--- week2/WorldTrotter/WorldTrotter/MapViewController.swift
+++ week2/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -16,29 +16,66 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
     var latitudeTextFeild : UITextField!
     var longitudeTextFeild : UITextField!
     var setAnnotationButton : UIButton!
+    var tourLocationsPinned : UIButton!
     let locationManager: CLLocationManager = CLLocationManager()
+    var currentPinIndex = 0
     
     override func loadView() {
         super.loadView()
         view.backgroundColor = UIColor.white
-        print("loadView")
-        createMapView()
-        createSegmentedControl()
-        createCurrentPositionButton()
+    
+        mapView = MKMapView()
+        mapView.delegate = self
+        mapView.isZoomEnabled = true
+        mapView.showsUserLocation = true
+        
+        latitudeTextFeild = UITextField()
+        longitudeTextFeild = UITextField()
+        latitudeTextFeild.placeholder = "위도 입력: "
+        longitudeTextFeild.placeholder = "경도 입력: "
+        
+        setAnnotationButton = UIButton(type: .system)
+        setAnnotationButton.setTitle("핀 추가", for: .normal)
+        setAnnotationButton.addTarget(self, action: #selector(addAnnotation(button:)), for: .touchUpInside)
+        setAnnotationButton.backgroundColor = UIColor.darkGray
+        
+        tourLocationsPinned = UIButton(type: .system)
+        tourLocationsPinned.setTitle("핀 순회", for: .normal)
+        tourLocationsPinned.addTarget(self, action: #selector(tour(button:)), for: .touchUpInside)
+        tourLocationsPinned.backgroundColor = UIColor.green
+        
+        
+        segmentedControl = UISegmentedControl(items: ["Standard","Hybrid","Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged) , for: .valueChanged)
+        
+
+        currentPositionButton = UIButton(type: .system)
+        currentPositionButton.backgroundColor = UIColor.gray
+        currentPositionButton.setTitle("현재위치", for: UIControlState.normal)
+        currentPositionButton.addTarget(self, action: #selector(annotateCurrentPositionAndZoom), for: .touchUpInside)
+        
+        view.addSubview(setAnnotationButton)
+        view.addSubview(latitudeTextFeild)
+        view.addSubview(longitudeTextFeild)
+        view.addSubview(mapView)
+        view.addSubview(segmentedControl)
+        view.addSubview(currentPositionButton)
+        view.addSubview(tourLocationsPinned)
+        
         createConstraint()
     }
-//    func createTextField() {
-//        latitudeTextFeild = UITextField()
-//        longitudeTextFeild = UITextField()
-//    }
+   
     
     func createConstraint() {
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         mapView.translatesAutoresizingMaskIntoConstraints = false
         currentPositionButton.translatesAutoresizingMaskIntoConstraints = false
-//        latitudeTextFeild.translatesAutoresizingMaskIntoConstraints = false
-//        longitudeTextFeild.translatesAutoresizingMaskIntoConstraints = false
-//        setAnnotationButton.translatesAutoresizingMaskIntoConstraints = false
+        latitudeTextFeild.translatesAutoresizingMaskIntoConstraints = false
+        longitudeTextFeild.translatesAutoresizingMaskIntoConstraints = false
+        setAnnotationButton.translatesAutoresizingMaskIntoConstraints = false
+        tourLocationsPinned.translatesAutoresizingMaskIntoConstraints = false
         
         segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8.0).isActive = true
         segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8.0).isActive = true
@@ -53,6 +90,23 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
         NSLayoutConstraint(item: currentPositionButton, attribute: .leading, relatedBy: .equal, toItem: mapView, attribute: .leading, multiplier: 1 , constant: 0.0).isActive = true
         NSLayoutConstraint(item: currentPositionButton, attribute: .width, relatedBy: .equal, toItem: mapView, attribute: .width, multiplier: 0.3, constant: 0).isActive = true
         NSLayoutConstraint(item: currentPositionButton, attribute: .bottom, relatedBy: .equal, toItem: bottomLayoutGuide, attribute: .top, multiplier: 1, constant: -8.0).isActive = true
+        
+        latitudeTextFeild.topAnchor.constraint(equalTo: currentPositionButton.topAnchor).isActive = true
+        latitudeTextFeild.leadingAnchor.constraint(equalTo: currentPositionButton.trailingAnchor, constant: 8.0).isActive = true
+        latitudeTextFeild.trailingAnchor.constraint(equalTo: mapView.trailingAnchor).isActive = true
+        
+        longitudeTextFeild.centerYAnchor.constraint(equalTo: currentPositionButton.centerYAnchor).isActive = true
+        longitudeTextFeild.leadingAnchor.constraint(equalTo: latitudeTextFeild.leadingAnchor).isActive = true
+        longitudeTextFeild.heightAnchor.constraint(equalTo: latitudeTextFeild.heightAnchor).isActive = true
+        longitudeTextFeild.widthAnchor.constraint(equalTo: latitudeTextFeild.widthAnchor).isActive = true
+        
+        setAnnotationButton.leadingAnchor.constraint(equalTo: latitudeTextFeild.leadingAnchor).isActive = true
+        setAnnotationButton.bottomAnchor.constraint(equalTo: currentPositionButton.bottomAnchor).isActive = true
+        setAnnotationButton.widthAnchor.constraint(equalTo: latitudeTextFeild.widthAnchor, multiplier: 0.4).isActive = true
+        
+        tourLocationsPinned.trailingAnchor.constraint(equalTo: mapView.trailingAnchor).isActive = true
+        tourLocationsPinned.widthAnchor.constraint(equalTo: setAnnotationButton.widthAnchor).isActive = true
+        tourLocationsPinned.bottomAnchor.constraint(equalTo: setAnnotationButton.bottomAnchor).isActive = true
     }
 
     func locationManagerConfiguration() {
@@ -60,32 +114,6 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-    }
-    
-    func createMapView() {
-        self.mapView = MKMapView()
-        self.mapView.delegate = self
-        self.mapView.isZoomEnabled = true
-        mapView.showsUserLocation = true
-        self.view.addSubview(mapView)
-    }
-    
-    func createSegmentedControl() {
-        segmentedControl = UISegmentedControl(items: ["Standard","Hybrid","Satellite"])
-        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
-        segmentedControl.selectedSegmentIndex = 0
-        segmentedControl.addTarget(self, action: #selector(mapTypeChanged) , for: .valueChanged)
-        view.addSubview(segmentedControl)
-    }
-    
-    func createCurrentPositionButton() {
-        currentPositionButton = UIButton()
-        currentPositionButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
-        currentPositionButton.setTitleColor(UIColor.red, for: UIControlState.highlighted)
-        currentPositionButton.backgroundColor = UIColor.yellow
-        currentPositionButton.setTitle("현재위치", for: UIControlState.normal)
-        currentPositionButton.addTarget(self, action: #selector(annotateCurrentPositionAndZoom), for: .touchUpInside)
-        view.addSubview(currentPositionButton)
     }
     
     override func viewDidLoad() {
@@ -109,6 +137,38 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
         default:
             break
         }
+    }
+    
+    func goLocation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue: CLLocationDegrees, delta span : Double)
+    -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegionMake(pLocation, spanValue)
+        mapView.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    func addAnnotation(button: UIButton) {
+        //예외처리 필요
+        let latitudeValue = CLLocationDegrees(latitudeTextFeild.text!)!
+        let longitudeValue = CLLocationDegrees(longitudeTextFeild.text!)!
+        setAnnotation(latitude:latitudeValue , longitude: longitudeValue, delta: 1, title: "추가해야한", subtitle: "추가해야함")
+    }
+    
+    func setAnnotation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue: CLLocationDegrees,
+                       delta span : Double, title strTitle: String, subtitle strSubtitle : String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitude: latitudeValue, longitude: longitudeValue, delta: span)
+        annotation.title = "strTitle"
+        annotation.subtitle = "strSubTitle"
+        mapView.addAnnotation(annotation)
+    }
+    
+    func tour(button: UIButton) {
+        currentPinIndex %= (mapView.annotations.count - 1)
+        print(currentPinIndex)
+        _ = goLocation(latitude: mapView.annotations[currentPinIndex].coordinate.latitude, longitude: mapView.annotations[currentPinIndex].coordinate.longitude, delta: 1)
+        currentPinIndex += 1
     }
     
     func annotateCurrentPositionAndZoom() {
