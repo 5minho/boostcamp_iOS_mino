1c32aa1728aad529dd85863e19b7f8bcc907d1d5
minomi <5minhominho@gmail.com>
2017-07-14 03:03:32 +0900

    9장 시작

---
+++ week3/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by 오민호 on 2017. 7. 14..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController : UITableViewController {
+    var itemStore: ItemStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+}
