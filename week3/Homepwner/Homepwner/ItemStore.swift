ecfa88e6b5fd505cf63b3b6da5fcd2ff9f0bc1d4
minomi <5minhominho@gmail.com>
2017-07-16 06:08:11 +0900

    5장 동메달 과제

--- week3/Homepwner/Homepwner/ItemStore.swift
+++ week3/Homepwner/Homepwner/ItemStore.swift
@@ -11,15 +11,20 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
-    init() {
-        for _ in 0..<5 {
-            createItem()
+    init(empty : Bool = false) {
+        if (!empty) {
+            for _ in 0..<5 {
+                createItem()
+            }
         }
     }
+
     
     func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
         return newItem
     }
+    
+
 }
--- week3/Homepwner/Homepwner/ItemsViewController.swift
+++ week3/Homepwner/Homepwner/ItemsViewController.swift
@@ -10,26 +10,51 @@ import UIKit
 
 class ItemsViewController : UITableViewController {
     var itemStore: ItemStore!
+    var itemsArrayClassifiedBy50Dollor: [ItemStore]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        itemsArrayClassifiedBy50Dollor = [ItemStore(empty: true), ItemStore(empty: true)]
+        classifyBy50Dollor()
         let statusBarHeight = UIApplication.shared.statusBarFrame.height
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
     }
+    
+    func classifyBy50Dollor() {
+        for item in itemStore.allItems {
+            if item.valueInDollars > 50 {
+                itemsArrayClassifiedBy50Dollor[0].allItems.append(item)
+            } else {
+                itemsArrayClassifiedBy50Dollor[1].allItems.append(item)
+            }
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return itemsArrayClassifiedBy50Dollor.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Higher Than $50" : "others"
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        let item = itemStore.allItems[indexPath.row]
+//        let item = itemStore.allItems[indexPath.row]
+        let item = itemsArrayClassifiedBy50Dollor[indexPath.section].allItems[indexPath.row]
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return itemsArrayClassifiedBy50Dollor[section].allItems.count
     }
+    
+    
 }
