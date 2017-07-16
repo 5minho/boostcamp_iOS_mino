09cc53354900bba6340dfdb13b9ac55db4037170
minomi <5minhominho@gmail.com>
2017-07-16 14:18:09 +0900

    9장 폴더 이름 변경

---
+++ week3/HomepwnerCopied/Homepwner/ItemStore.swift
@@ -0,0 +1,30 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by 오민호 on 2017. 7. 14..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    init(empty : Bool = false) {
+        if (!empty) {
+            for _ in 0..<5 {
+                createItem()
+            }
+        }
+    }
+
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+    
+
+}
