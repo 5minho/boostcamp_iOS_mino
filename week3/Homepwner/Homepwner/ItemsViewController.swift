69d8d93b8729a292298a3e143b956e9e31bbc8f1
minomi <5minhominho@gmail.com>
2017-07-16 16:15:46 +0900

    10장 동메달 과제

--- week3/Homepwner/Homepwner/ItemsViewController.swift
+++ week3/Homepwner/Homepwner/ItemsViewController.swift
@@ -18,6 +18,7 @@ class ItemsViewController : UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
@@ -42,7 +43,7 @@ class ItemsViewController : UITableViewController {
             let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
             let cencelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
             ac.addAction(cencelAction)
-            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {
+            let deleteAction = UIAlertAction(title: "Remove", style: .destructive, handler: {
                 (action) -> Void in
                 self.itemStore.removeItem(item: item)
                 tableView.deleteRows(at: [indexPath], with: .automatic)
