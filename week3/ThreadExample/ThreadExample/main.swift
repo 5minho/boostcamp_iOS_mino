80001dee116995390f73369e13b498e221e1d089
minomi <5minhominho@gmail.com>
2017-07-17 04:39:44 +0900

    delete Thread

---
+++ week3/ThreadExample/ThreadExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ThreadExample
+//
+//  Created by 오민호 on 2017. 7. 17..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
