412cc85120dbffc8ef4988063f14add2306a9b32
minomi <5minhominho@gmail.com>
2017-07-17 16:19:56 +0900

    OperationQueue example

--- week3/ThreadExample/ThreadExample/main.swift
+++ week3/ThreadExample/ThreadExample/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+class MyOperation: Operation {
+    var index: Int?
+    
+    override func main() {
+        if let index = self.index {
+            print("From My Operation \(index)")
+        }
+    }
+    
+    init(index: Int) {
+        super.init()
+        self.index = index
+    }
+}
+
+class MyWork {
+    let queue = OperationQueue()
+    init() {
+        self.queue.addOperation(MyOperation(index: 0))
+        self.queue.addOperation(MyOperation(index: 1))
+        self.queue.addOperation(MyOperation(index: 2))
+        self.queue.addOperation(MyOperation(index: 3))
+        self.queue.addOperation(MyOperation(index: 4))
+    }
+}
+
+let work = MyWork()
+
+while true {
+    sleep(1)
+}
 
