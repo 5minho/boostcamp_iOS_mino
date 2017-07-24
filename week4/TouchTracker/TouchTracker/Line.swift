84a78291ea0c4fb560a140399b59bbc37eeff398
minomi <5minhominho@gmail.com>
2017-07-25 07:17:53 +0900

    17장 본문

---
+++ week4/TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by 오민호 on 2017. 7. 7..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
