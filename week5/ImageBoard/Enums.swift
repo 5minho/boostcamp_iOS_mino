a199c9cabfb84f7a90158ddd98e263194d154928
minomi <5minhominho@gmail.com>
2017-08-01 22:36:39 +0900

    enum 형 -> Enums.swift 로 이동

---
+++ week5/ImageBoard/Enums.swift
@@ -0,0 +1,48 @@
+//
+//  ImageResult.swift
+//  ImageBoard
+//
+//  Created by 오민호 on 2017. 8. 1..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum LoginResult {
+    case success(User)
+    case failure(Int, String)
+}
+
+enum ImageError : Error {
+    case imageCreationError
+}
+
+enum ImageSize {
+    case thumbnail
+    case detail
+}
+
+enum PostResult {
+    case success
+    case failure(Error)
+}
+
+
+enum ArticleResult {
+    case success([Article])
+    case failure(Error)
+}
+
+enum ImageBoardError : Error {
+    case invalidJSONData
+}
+
+enum EditResult {
+    case success
+    case failure
+}
